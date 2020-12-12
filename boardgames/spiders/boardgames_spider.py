4f2e0024b260d1b469386ac62037f624c80d158a
viviann <viviann.chan@gmail.com>
2020-12-12 10:27:02 +0800

    fixed csv

--- boardgames/spiders/boardgames_spider.py
+++ boardgames/spiders/boardgames_spider.py
@@ -8,8 +8,8 @@ class BoardgamesSpider(Spider):
 	start_urls = ['https://boardgamegeek.com/browse/boardgame/page/1']
 
 	def parse(self, response):
-		# num_pages = int(response.xpath('//a[@title="last page"]/text()').extract_first()[1:-1])
-		num_pages = 5
+		num_pages = int(response.xpath('//a[@title="last page"]/text()').extract_first()[1:-1])
+		# num_pages = 5
 		page_urls = [f'https://boardgamegeek.com/browse/boardgame/page/{i+1}' for i in range(num_pages)]
 
 		# print(page_urls)
@@ -20,7 +20,7 @@ class BoardgamesSpider(Spider):
 			# print("scraping page:"+ url)
 
 			# yield Request(url=url, callback = self.parse_game_page)
-			# yield Request(url=url, callback = self.parse_results_page)
+			yield Request(url=url, callback = self.parse_results_page)
 
 	# def parse_results_page(self, response):
 	# 	# game_urls = response.xpath('//td[@class="collection_thumbnail"]/a/@href').extract()
@@ -37,47 +37,48 @@ class BoardgamesSpider(Spider):
 	# 	for url in game_urls:
 	# 		yield Request(url=url, callback = self.parse_game_page)
 
-	#def parse_game_page(self, response):
+	# def parse_game_page(self, response):
+	def parse_results_page(self, response):	
 
-			games = response.xpath('//div[@id="collection"]//tr[@id="row_"]')
+		games = response.xpath('//div[@id="collection"]//tr[@id="row_"]')
 
-			for game in games:
-				title = game.xpath('.//div/a/text()').extract_first()
-				year = int(re.findall('\d+',game.xpath('.//div/span/text()').extract_first())[0])
-				try:
-					rank = int(game.xpath('.//td[@class="collection_rank"]/text()').extract()[1].strip())
-				except:
-					rank = game.xpath('.//td[@class="collection_rank"]//text()').extract_first().strip()
-				try:
-					geek_rating = float(game.xpath('.//td[@class="collection_bggrating"][1]/text()').extract_first().strip())
-				except:
-					geek_rating = game.xpath('.//td[@class="collection_bggrating"][1]/text()').extract_first().strip()
-				try:
-					avg_rating = float(game.xpath('.//td[@class="collection_bggrating"][2]/text()').extract_first().strip())
-				except:
-					avg_rating = game.xpath('.//td[@class="collection_bggrating"][2]/text()').extract_first().strip()
-				try:
-					num_rating = float(game.xpath('.//td[@class="collection_bggrating"][3]/text()').extract_first().strip())
-				except:
-					num_rating = game.xpath('.//td[@class="collection_bggrating"][2]/text()').extract_first().strip()
-				try:
-					description = game.xpath('.//p/text()').extract_first().strip()
-				except:
-					description = game.xpath('.//p/text()').extract_first()
-				url = game.xpath('.//td[@class="collection_thumbnail"]/a/@href').extract_first()
-				url = 'https://boardgamegeek.com' + url
+		for game in games:
+			title = game.xpath('.//div/a/text()').extract_first()
+			year = int(re.findall('\d+',game.xpath('.//div/span/text()').extract_first())[0])
+			try:
+				rank = int(game.xpath('.//td[@class="collection_rank"]/text()').extract()[1].strip())
+			except:
+				rank = game.xpath('.//td[@class="collection_rank"]//text()').extract_first().strip()
+			try:
+				geek_rating = float(game.xpath('.//td[@class="collection_bggrating"][1]/text()').extract_first().strip())
+			except:
+				geek_rating = game.xpath('.//td[@class="collection_bggrating"][1]/text()').extract_first().strip()
+			try:
+				avg_rating = float(game.xpath('.//td[@class="collection_bggrating"][2]/text()').extract_first().strip())
+			except:
+				avg_rating = game.xpath('.//td[@class="collection_bggrating"][2]/text()').extract_first().strip()
+			try:
+				num_rating = float(game.xpath('.//td[@class="collection_bggrating"][3]/text()').extract_first().strip())
+			except:
+				num_rating = game.xpath('.//td[@class="collection_bggrating"][2]/text()').extract_first().strip()
+			try:
+				description = game.xpath('.//p/text()').extract_first().strip()
+			except:
+				description = game.xpath('.//p/text()').extract_first()
+			url = game.xpath('.//td[@class="collection_thumbnail"]/a/@href').extract_first()
+			url = 'https://boardgamegeek.com' + url
 
-				item = BoardgamesItem()
-				item['title'] = title
-				item['year'] = year
-				item['rank'] = rank
-				item['geek_rating'] = geek_rating
-				item['avg_rating'] = avg_rating
-				item['num_rating'] = num_rating
-				item['description'] = description
-				item['url'] = url
+			item = BoardgamesItem()
+			item['title'] = title
+			item['year'] = year
+			item['rank'] = rank
+			item['geek_rating'] = geek_rating
+			item['avg_rating'] = avg_rating
+			item['num_rating'] = num_rating
+			item['description'] = description
+			item['url'] = url
 
-				yield item
+			yield item
 
 
 
