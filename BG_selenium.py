939a283aab6f3e9cccd65e1e8349f9a6df67ee41
viviann <viviann.chan@gmail.com>
2020-12-13 19:55:07 +0800

    selenium script

--- BG_selenium.py
+++ BG_selenium.py
@@ -3,46 +3,55 @@ import time
 import re
 import csv
 
-driver = webdriver.Chrome()
+urls = ["https://boardgamegeek.com/boardgame/161936/pandemic-legacy-season-1","https://boardgamegeek.com/boardgame/174430/gloomhaven","https://boardgamegeek.com/boardgame/254640/just-one","https://boardgamegeek.com/boardgame/72125/eclipse","https://boardgamegeek.com/boardgame/463/magic-gathering"]
 
 # driver.get("https://boardgamegeek.com/boardgame/161936/pandemic-legacy-season-1")
-driver.get("https://boardgamegeek.com/boardgame/174430/gloomhaven")
+# driver.get("https://boardgamegeek.com/boardgame/174430/gloomhaven")
 # driver.get("https://boardgamegeek.com/boardgame/254640/just-one")
+# driver.get("https://boardgamegeek.com/boardgame/72125/eclipse")
+# driver.get("https://boardgamegeek.com/boardgame/463/magic-gathering")
+csv_file = open('game_details.csv','w', encoding='utf-8', newline='')
+writer = csv.writer(csv_file)
+writer.writerow(['title','age','complexity','time','min_player','max_player','category'])
 
-#game = {}
 
-#title = driver.find_element_by_xpath('//h1/a').text
-#complexity = driver.find_element_by_xpath('//ul[@class="gameplay"]/li[4]/div/span[2]/span[1]').text
-test = 
+for url in urls:
+	driver = webdriver.Chrome()
+	driver.get(url)
 
-print(test)
+	title = driver.find_element_by_xpath('//h1/a').text
+	age = driver.find_element_by_xpath('//ul[@class="gameplay"]/li[3]/div/span').text
+	complexity = driver.find_element_by_xpath('//ul[@class="gameplay"]/li[4]/div/span[2]/span[1]').text
+	min_player = driver.find_element_by_xpath('//ul[@class="gameplay"]/li[1]/div/span/span').text
+	try:
+		max_player = driver.find_element_by_xpath('//ul[@class="gameplay"]/li[1]/div/span/span[2]').text[1]
+	except:
+		max_player = min_player
+	time = driver.find_element_by_xpath('//ul[@class="gameplay"]/li[2]/div/span/span/span').text
 
-# game_dict['title'] = title
+	i=1
+	category=[]
+	while i<8:
+		try:
+			# item = driver.find_element_by_xpath(f'//div[@class="feature-description"]/span[{i}]/a').text
+			item = driver.find_element_by_xpath(f'//li[@class="feature"]/div[2]/span[{i}]/a').text
+			category.append(item)
+		except:
+			break
+		i+=1
 
-			# OPTIONAL: How can we deal with the "read more" button?
-			
-			# Use relative xpath to locate text, username, date_published, rating.
-			# Your code here
+	game = {}
+	game['title'] = title
+	game['age'] = int(re.findall('\d+',age)[0])
+	game['complexity'] = float(complexity)
+	game['min_player'] = int(min_player)
+	game['max_player'] = int(max_player)
+	game['time'] = int(time)
+	game['category'] = category
+	print(game)
 
-			# Uncomment the following lines once you verified the xpath of different fields
-			
-			# review_dict['title'] = title
-			# review_dict['text'] = text
-			# review_dict['username'] = username
-			# review_dict['date_published'] = date_published
-			# review_dict['rating'] = rating
+	writer.writerow(game.values())
 
-		# We need to scroll to the bottom of the page because the button is not in the current view yet.
-	# 	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+	driver.close()
 
-	# 	# Locate the next button element on the page and then call `button.click()` to click it.
-	# 	button = driver.find_element_by_xpath('//li[@class="nextClick displayInlineBlock padLeft5 "]')
-	# 	button.click()
-	# 	time.sleep(2)
 
-	# except Exception as e:
-	# 	print(e)
-	# 	driver.close()
-	# 	break
-
-driver.close()
